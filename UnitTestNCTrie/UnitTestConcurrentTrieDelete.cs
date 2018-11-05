6bd0237ae778edbc0ce719f2f142a2d7df276802
jsbattig <jsbattig@gmail.com>
2018-11-04 23:54:32 -0300

    Renamed project and added a couple more tests

---
+++ UnitTestNCTrie/UnitTestConcurrentTrieDelete.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JSB.Collections.ConcurrentTrie;
+
+namespace UnitTestNCTrie
+{
+  [TestClass]
+  public class UnitTestConcurrentTrieDelete
+  {
+    [TestMethod]
+    public void TestConcurrentTrieDelete()
+    {
+      ConcurrentTrieDictionary< Object, Object > bt = new ConcurrentTrieDictionary<Object, Object>();
+
+      for (int i = 0; i < 10000; i++)
+      {
+        TestHelper.assertEquals(null, bt.put(i, i));
+        Object lookup = bt.lookup(i);
+        TestHelper.assertEquals(i, lookup);
+      }
+
+      checkAddInsert(bt, 536);
+      checkAddInsert(bt, 4341);
+      checkAddInsert(bt, 8437);
+
+      for (int i = 0; i < 10000; i++)
+      {
+        bool removed = null != bt.remove(i);
+        TestHelper.assertEquals(true, removed);
+        Object lookup = bt.lookup(i);
+        TestHelper.assertEquals(null, lookup);
+      }
+    }
+
+    private static void checkAddInsert(ConcurrentTrieDictionary<Object, Object> bt, int k)
+    {
+      int v = k;
+      bt.remove(v);
+      Object foundV = bt.lookup(v);
+      TestHelper.assertEquals(null, foundV);
+      TestHelper.assertEquals(null, bt.put(v, v));
+      foundV = bt.lookup(v);
+      TestHelper.assertEquals(v, foundV);
+
+      TestHelper.assertEquals(v, bt.put(v, -1));
+      TestHelper.assertEquals(-1, bt.put(v, v));
+    }
+  }
+}
---
+++ UnitTestNCTrie/UnitTestConcurrentTrieHashCollisions.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JSB.Collections.ConcurrentTrie;
+
+namespace UnitTestNCTrie
+{
+  [TestClass]
+  public class UnitTestConcurrentTrieHashCollisions
+  {
+    [TestMethod]    
+    public void TestConcurrentTrieHashCollisions()
+    {
+      ConcurrentTrieDictionary< Object, Object > bt = new ConcurrentTrieDictionary<Object, Object>();
+
+      insertStrings(bt);
+      insertChars(bt);
+      insertInts(bt);
+      insertBytes(bt);
+
+      removeStrings(bt);
+      removeChars(bt);
+      removeInts(bt);
+      removeBytes(bt);
+
+      insertStrings(bt);
+      insertInts(bt);
+      insertBytes(bt);
+      insertChars(bt);
+
+      removeBytes(bt);
+      removeStrings(bt);
+      removeChars(bt);
+      removeInts(bt);
+
+      insertStrings(bt);
+      insertInts(bt);
+      insertBytes(bt);
+      insertChars(bt);
+
+      removeStrings(bt);
+      removeChars(bt);
+      removeInts(bt);
+      removeBytes(bt);
+
+      insertStrings(bt);
+      insertInts(bt);
+      insertBytes(bt);
+      insertChars(bt);
+
+      removeChars(bt);
+      removeInts(bt);
+      removeBytes(bt);
+      removeStrings(bt);
+
+      insertStrings(bt);
+      insertInts(bt);
+      insertBytes(bt);
+      insertChars(bt);
+
+      removeInts(bt);
+      removeBytes(bt);
+      removeStrings(bt);
+      removeChars(bt);
+    }
+
+    private static void insertChars(ConcurrentTrieDictionary<Object, Object> bt)
+    {
+      TestHelper.assertEquals(null, bt.put('a', 'a'));
+      TestHelper.assertEquals(null, bt.put('b', 'b'));
+      TestHelper.assertEquals(null, bt.put('c', 'c'));
+      TestHelper.assertEquals(null, bt.put('d', 'd'));
+      TestHelper.assertEquals(null, bt.put('e', 'e'));
+
+      TestHelper.assertEquals('a', bt.put('a', 'a'));
+      TestHelper.assertEquals('b', bt.put('b', 'b'));
+      TestHelper.assertEquals('c', bt.put('c', 'c'));
+      TestHelper.assertEquals('d', bt.put('d', 'd'));
+      TestHelper.assertEquals('e', bt.put('e', 'e'));
+    }
+
+    private static void insertStrings(ConcurrentTrieDictionary<Object, Object> bt)
+    {
+      TestHelper.assertEquals(null, bt.put("a", "a"));
+      TestHelper.assertEquals(null, bt.put("b", "b"));
+      TestHelper.assertEquals(null, bt.put("c", "c"));
+      TestHelper.assertEquals(null, bt.put("d", "d"));
+      TestHelper.assertEquals(null, bt.put("e", "e"));
+
+      TestHelper.assertEquals("a", bt.put("a", "a"));
+      TestHelper.assertEquals("b", bt.put("b", "b"));
+      TestHelper.assertEquals("c", bt.put("c", "c"));
+      TestHelper.assertEquals("d", bt.put("d", "d"));
+      TestHelper.assertEquals("e", bt.put("e", "e"));
+    }
+
+    private static void insertBytes(ConcurrentTrieDictionary<Object, Object> bt)
+    {
+      for (byte i = 0; i < 128 && i >= 0; i++)
+      {
+        byte bigB = i;
+        TestHelper.assertEquals(null, bt.put(bigB, bigB));
+        TestHelper.assertEquals(bigB, bt.put(bigB, bigB));
+      }
+    }
+
+    private static void insertInts(ConcurrentTrieDictionary<Object, Object> bt)
+    {
+      for (int i = 0; i < 128; i++)
+      {
+        int bigI = i;
+        TestHelper.assertEquals(null, bt.put(bigI, bigI));
+        TestHelper.assertEquals(bigI, bt.put(bigI, bigI));
+      }
+    }
+
+    private static void removeChars(ConcurrentTrieDictionary<Object, Object> bt)
+    {
+      TestHelper.assertTrue(null != bt.lookup('a'));
+      TestHelper.assertTrue(null != bt.lookup('b'));
+      TestHelper.assertTrue(null != bt.lookup('c'));
+      TestHelper.assertTrue(null != bt.lookup('d'));
+      TestHelper.assertTrue(null != bt.lookup('e'));
+
+      TestHelper.assertTrue(null != bt.remove('a'));
+      TestHelper.assertTrue(null != bt.remove('b'));
+      TestHelper.assertTrue(null != bt.remove('c'));
+      TestHelper.assertTrue(null != bt.remove('d'));
+      TestHelper.assertTrue(null != bt.remove('e'));
+
+      TestHelper.assertFalse(null != bt.remove('a'));
+      TestHelper.assertFalse(null != bt.remove('b'));
+      TestHelper.assertFalse(null != bt.remove('c'));
+      TestHelper.assertFalse(null != bt.remove('d'));
+      TestHelper.assertFalse(null != bt.remove('e'));
+
+      TestHelper.assertTrue(null == bt.lookup('a'));
+      TestHelper.assertTrue(null == bt.lookup('b'));
+      TestHelper.assertTrue(null == bt.lookup('c'));
+      TestHelper.assertTrue(null == bt.lookup('d'));
+      TestHelper.assertTrue(null == bt.lookup('e'));
+    }
+
+    private static void removeStrings(ConcurrentTrieDictionary<Object, Object> bt)
+    {
+      TestHelper.assertTrue(null != bt.lookup("a"));
+      TestHelper.assertTrue(null != bt.lookup("b"));
+      TestHelper.assertTrue(null != bt.lookup("c"));
+      TestHelper.assertTrue(null != bt.lookup("d"));
+      TestHelper.assertTrue(null != bt.lookup("e"));
+
+      TestHelper.assertTrue(null != bt.remove("a"));
+      TestHelper.assertTrue(null != bt.remove("b"));
+      TestHelper.assertTrue(null != bt.remove("c"));
+      TestHelper.assertTrue(null != bt.remove("d"));
+      TestHelper.assertTrue(null != bt.remove("e"));
+
+      TestHelper.assertFalse(null != bt.remove("a"));
+      TestHelper.assertFalse(null != bt.remove("b"));
+      TestHelper.assertFalse(null != bt.remove("c"));
+      TestHelper.assertFalse(null != bt.remove("d"));
+      TestHelper.assertFalse(null != bt.remove("e"));
+
+      TestHelper.assertTrue(null == bt.lookup("a"));
+      TestHelper.assertTrue(null == bt.lookup("b"));
+      TestHelper.assertTrue(null == bt.lookup("c"));
+      TestHelper.assertTrue(null == bt.lookup("d"));
+      TestHelper.assertTrue(null == bt.lookup("e"));
+    }
+
+    private static void removeInts(ConcurrentTrieDictionary<Object, Object> bt)
+    {
+      for (int i = 0; i < 128; i++)
+      {
+        int bigI = i;
+        TestHelper.assertTrue(null != bt.lookup(bigI));
+        TestHelper.assertTrue(null != bt.remove(bigI));
+        TestHelper.assertFalse(null != bt.remove(bigI));
+        TestHelper.assertTrue(null == bt.lookup(bigI));
+      }
+    }
+
+    private static void removeBytes(ConcurrentTrieDictionary<Object, Object> bt)
+    {
+      for (byte i = 0; i < 128 && i >= 0; i++)
+      {
+        byte bigB = i;
+        TestHelper.assertTrue(null != bt.lookup(bigB));
+        TestHelper.assertTrue(null != bt.remove(bigB));
+        TestHelper.assertFalse(null != bt.remove(bigB));
+        TestHelper.assertTrue(null == bt.lookup(bigB));
+      }
+    }
+  }
+}
