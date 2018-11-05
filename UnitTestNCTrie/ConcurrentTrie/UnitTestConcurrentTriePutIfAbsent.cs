33aae8882fc8d1ed111d771c96fc633ef7f2033b
jsbattig <jsbattig@gmail.com>
2018-11-05 00:12:47 -0300

    Added PutIfAbsent tests

---
+++ UnitTestNCTrie/ConcurrentTrie/UnitTestConcurrentTriePutIfAbsent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JSB.Collections.ConcurrentTrie;
+
+namespace UnitTestNCTrie.ConcurrentTrie
+{
+  [TestClass]
+  public class UnitTestConcurrentTriePutIfAbsent
+  {
+    private static int COUNT = 50 * 1000;
+
+    [TestMethod]
+    public void TestConcurrentTriePutIfAbsent()
+    {
+      ConcurrentTrieDictionary< Object, Object > map = new ConcurrentTrieDictionary<Object, Object>();
+
+      for (int i = 0; i < COUNT; i++)
+      {
+        TestHelper.assertTrue(null == map.putIfAbsent(i, i));
+        TestHelper.assertTrue(i.Equals(map.putIfAbsent(i, i)));
+      }
+    }
+  }
+}
